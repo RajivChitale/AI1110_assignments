e01e28b9dbdb4f051bec7a91249e54373cbce39a
cs21btech11051Rajiv <102544281+cs21btech11051Rajiv@users.noreply.github.com>
2022-03-29 06:01:47 -0700

    Added python code for verification of solution

---
+++ assign1/verify.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#find matrix X from B
+B= np.matrix('1 1; 8 3')
+X= B*B- 4*B
+
+Solution = np.matrix('1;10')
+L= X*Solution
+R= np.matrix('5;50')
+
+print('Verifying whether LHS=RHS for a=1 and b=10,')
+print('\nLHS=')
+print(L)
+print('\nRHS=')
+print(R)
